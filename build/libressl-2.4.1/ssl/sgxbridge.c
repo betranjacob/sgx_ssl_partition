72a1ba5991dbd843d6060731b8255550559bc709
Ahmed Awad <b00042183@aus.edu>
2016-07-28 11:40:04 +0100

    Fixed formatting and programming error

--- build/libressl-2.4.1/ssl/sgxbridge.c
+++ build/libressl-2.4.1/ssl/sgxbridge.c
@@ -13,241 +13,235 @@
 #define RB_MODE_RD 0
 #define RB_MODE_WR 1
 
-
 char TMP_DIRECTORY_CONF[] = "/tmp/ipc_conf";
 char TMP_DIRECTORY_RUN[] = "/tmp/ipc_run";
-char TMP_FILE_NUMBER_FMT[] =  "/pipe_";
-
-
+char TMP_FILE_NUMBER_FMT[] = "/pipe_";
 
 int fd_sgx_ssl = -1;
 int fd_ssl_sgx = -1;
 
-
 int
 opensgx_pipe_init(int flag_dir)
 {
-    int ret;
+  int ret;
 
-    if(flag_dir == 0)
-        ret = mkdir(TMP_DIRECTORY_CONF, 0770);
-    else if(flag_dir == 1)
-        ret = mkdir(TMP_DIRECTORY_RUN, 0770);
+  if (flag_dir == 0)
+    ret = mkdir(TMP_DIRECTORY_CONF, 0770);
+  else if (flag_dir == 1)
+    ret = mkdir(TMP_DIRECTORY_RUN, 0770);
 
-    if(ret == -1)
-    {
-        if(errno != EEXIST) {
-            fprintf(stderr, "Fail to mkdir");
-            return -1;
-        }
+  if (ret == -1) {
+    if (errno != EEXIST) {
+      fprintf(stderr, "Fail to mkdir");
+      return -1;
     }
-    return 0;
+  }
+  return 0;
 }
 
 int
-opensgx_pipe_open(char *unique_id, int is_write, int flag_dir)
+opensgx_pipe_open(char* unique_id, int is_write, int flag_dir)
 {
-    char name_buf[NAME_BUF_SIZE];
+  char name_buf[NAME_BUF_SIZE];
 
-    if (flag_dir == 0) {
-        strcpy(name_buf, TMP_DIRECTORY_CONF);
-        strcpy(name_buf+strlen(name_buf), TMP_FILE_NUMBER_FMT);
-        strcpy(name_buf+strlen(name_buf), unique_id);
+  if (flag_dir == 0) {
+    strcpy(name_buf, TMP_DIRECTORY_CONF);
+    strcpy(name_buf + strlen(name_buf), TMP_FILE_NUMBER_FMT);
+    strcpy(name_buf + strlen(name_buf), unique_id);
+  } else if (flag_dir == 1) {
+    strcpy(name_buf, TMP_DIRECTORY_RUN);
+    strcpy(name_buf + strlen(name_buf), TMP_FILE_NUMBER_FMT);
+    strcpy(name_buf + strlen(name_buf), unique_id);
+  }
+
+  int ret = mknod(name_buf, S_IFIFO | 0770, 0);
+  if (ret == -1) {
+    if (errno != EEXIST) {
+      fprintf(stderr, "Fail to mknod");
+      return -1;
     }
-    else if (flag_dir == 1) {
-        strcpy(name_buf, TMP_DIRECTORY_RUN);
-        strcpy(name_buf+strlen(name_buf), TMP_FILE_NUMBER_FMT);
-        strcpy(name_buf+strlen(name_buf), unique_id);
-    }
+  }
 
-    int ret = mknod(name_buf, S_IFIFO | 0770, 0);
-    if(ret == -1)
-    {
-        if(errno != EEXIST) {
-        	fprintf(stderr, "Fail to mknod");
-        	return -1;
-        }
-    }
+  int flag = O_ASYNC;
+  if (is_write)
+    flag |= O_WRONLY;
+  else
+    flag |= O_RDONLY;
 
-    int flag = O_ASYNC;
-    if(is_write)
-        flag |= O_WRONLY;
-    else
-        flag |= O_RDONLY;
+  int fd = open(name_buf, flag);
 
-    int fd = open(name_buf, flag);
+  if (fd == -1) {
+    fprintf(stderr, "Fail to open()");
+    return -1;
+  }
 
-    if(fd == -1)
-    {
-    	fprintf(stderr, "Fail to open()");
-        return -1;
-    }
-
-    return fd;
+  return fd;
 }
 
 void
 sgxbridge_pipe_read(int len, char* data)
 {
-    int fd = fd_ssl_sgx;
-    
-    #ifdef SGX_ENCLAVE
-        fd = fd_sgx_ssl;
-    #endif
+  int fd = fd_ssl_sgx;
 
-    read(fd_sgx_ssl, data, len);
+#ifdef SGX_ENCLAVE
+  fd = fd_sgx_ssl;
+#endif
+
+  read(fd, data, len);
 }
 
 sgxbridge_pipe_write(char* data, int len)
 {
-    int fd = fd_ssl_sgx;
-    
-    #ifdef SGX_ENCLAVE
-        fd = fd_sgx_ssl;
-    #endif
+  int fd = fd_ssl_sgx;
 
-    write(fd, data, len);
+#ifdef SGX_ENCLAVE
+  fd = fd_sgx_ssl;
+#endif
+
+  write(fd, data, len);
 }
 
 void
 sgxbridge_pipe_write_cmd(char* cmd, int len, char* data)
 {
-    int fd = fd_ssl_sgx;
+  int fd = fd_ssl_sgx;
 
-    #ifdef SGX_ENCLAVE
-        fd = fd_sgx_ssl;
-    #endif
+#ifdef SGX_ENCLAVE
+  fd = fd_sgx_ssl;
+#endif
 
-    printf("sgxbridge_pipe_write, cmd: %s, len: %d\n", cmd, len);
-    print_hex(data, len);
+  printf("sgxbridge_pipe_write, cmd: %s, len: %d\n", cmd, len);
+  print_hex(data, len);
 
-    int cmd_len = strlen(cmd);
+  int cmd_len = strlen(cmd);
 
-    write(fd, &cmd_len, sizeof(int));
-    write(fd, cmd, cmd_len+1);
+  write(fd, &cmd_len, sizeof(int));
+  write(fd, cmd, cmd_len + 1);
 
-    write(fd, &len, sizeof(int));
-    if(len > 0)
-        write(fd, data, len);
+  write(fd, &len, sizeof(int));
+  if (len > 0)
+    write(fd, data, len);
 }
 
 int
 sgxbridge_init()
 {
-    // default for ssl library
-    int mode_sgx_ssl = RB_MODE_RD;
-    int mode_ssl_sgx = RB_MODE_WR;
+  // default for ssl library
+  int mode_sgx_ssl = RB_MODE_RD;
+  int mode_ssl_sgx = RB_MODE_WR;
 
-    #ifdef SGX_ENCLAVE
-        mode_sgx_ssl = RB_MODE_WR;
-        mode_ssl_sgx = RB_MODE_RD;
-    #endif
+#ifdef SGX_ENCLAVE
+  mode_sgx_ssl = RB_MODE_WR;
+  mode_ssl_sgx = RB_MODE_RD;
+#endif
 
-    if(opensgx_pipe_init(0) < 0) {
-        fprintf(stderr, "%s - %s Pipe Init() failed \n", __FILE__, __func__);
-        return -1;
-    }
+  if (opensgx_pipe_init(0) < 0) {
+    fprintf(stderr, "%s - %s Pipe Init() failed \n", __FILE__, __func__);
+    return -1;
+  }
 
-    if((fd_sgx_ssl = opensgx_pipe_open("sgx_ssl", mode_sgx_ssl, 0)) < 0) {
-    	fprintf(stderr, "%s - %s Read Pipe Open() failed \n", __FILE__, __func__);
-        return -1;
-    }
+  if ((fd_sgx_ssl = opensgx_pipe_open("sgx_ssl", mode_sgx_ssl, 0)) < 0) {
+    fprintf(stderr, "%s - %s Read Pipe Open() failed \n", __FILE__, __func__);
+    return -1;
+  }
 
-    if((fd_ssl_sgx = opensgx_pipe_open("ssl_sgx", mode_ssl_sgx, 0)) < 0) {
-    	fprintf(stderr, "%s - %s Write Pipe Open() failed \n", __FILE__, __func__);
-        return -1;
-    }
-    
-    return 0;
+  if ((fd_ssl_sgx = opensgx_pipe_open("ssl_sgx", mode_ssl_sgx, 0)) < 0) {
+    fprintf(stderr, "%s - %s Write Pipe Open() failed \n", __FILE__, __func__);
+    return -1;
+  }
+
+  return 0;
 }
 
 int
-sgxbridge_fetch_operation(int *cmd_len, char *cmd, int *data_len, char *data)
+sgxbridge_fetch_operation(int* cmd_len, char* cmd, int* data_len, char* data)
 {
-    int fd = fd_sgx_ssl;
+  int fd = fd_sgx_ssl;
 
-    #ifdef SGX_ENCLAVE
-    fd = fd_ssl_sgx;
-    #endif
+#ifdef SGX_ENCLAVE
+  fd = fd_ssl_sgx;
+#endif
 
-    // read in comand length
-    if (read(fd, cmd_len, sizeof(int)) > 0) {
-        if( *cmd_len > CMD_MAX_BUF_SIZE) {
-            return 0;
-        }
-        // *cmd = malloc(sizeof(char) * (*cmd_len+1));
-        // data = NULL;
-
-        // read in command
-        read(fd, cmd, *cmd_len+1);
-
-        // read in data
-        if (read(fd, data_len, sizeof(int)) > 0) {
-            if(*data_len > CMD_MAX_BUF_SIZE) {
-                return 0;
-            }
-
-            if(*data_len > 0) {
-                // *data = malloc(sizeof(char) * (*data_len));
-                read(fd, data, *data_len);
-            }
-
-            printf("fetch_operation, cmd: %s, len: %d\n", cmd, *data_len);
-            print_hex(data, *data_len);
-
-            return 1;
-        }
+  // read in comand length
+  if (read(fd, cmd_len, sizeof(int)) > 0) {
+    if (*cmd_len > CMD_MAX_BUF_SIZE) {
+      return 0;
     }
-    
-    return 0;
+    // *cmd = malloc(sizeof(char) * (*cmd_len+1));
+    // data = NULL;
+
+    // read in command
+    read(fd, cmd, *cmd_len + 1);
+
+    // read in data
+    if (read(fd, data_len, sizeof(int)) > 0) {
+      if (*data_len > CMD_MAX_BUF_SIZE) {
+        return 0;
+      }
+
+      if (*data_len > 0) {
+        // *data = malloc(sizeof(char) * (*data_len));
+        read(fd, data, *data_len);
+      }
+
+      printf("fetch_operation, cmd: %s, len: %d\n", cmd, *data_len);
+      print_hex(data, *data_len);
+
+      return 1;
+    }
+  }
+
+  return 0;
 }
 
 void
-print_hex(unsigned char *buf, int len)
+print_hex(unsigned char* buf, int len)
 {
-    int cnt;
-    for (cnt = 0; cnt < len; cnt++)
-    {
-        printf("%02X", buf[cnt]);
-    }
-    printf("\n\r");
-    fflush(stdout);
+  int cnt;
+  for (cnt = 0; cnt < len; cnt++) {
+    printf("%02X", buf[cnt]);
+  }
+  printf("\n\r");
+  fflush(stdout);
 }
 
 void
-sgxbridge_generate_server_random(void* buf, int nbytes) {
-    printf("generate_server_random\n");
+sgxbridge_generate_server_random(void* buf, int nbytes)
+{
+  printf("generate_server_random\n");
 
-    sgxbridge_pipe_write_cmd(CMD_SRV_RAND, sizeof(int), &nbytes);
-    read(fd_sgx_ssl, buf, nbytes);
+  sgxbridge_pipe_write_cmd(CMD_SRV_RAND, sizeof(int), &nbytes);
+  read(fd_sgx_ssl, buf, nbytes);
 
-    printf("server_random:\n");
-    print_hex((unsigned char*) buf, nbytes);
+  printf("server_random:\n");
+  print_hex((unsigned char*)buf, nbytes);
 }
 
 int
-sgxbridge_get_master_secret(unsigned char *buf) {
-    sgxbridge_pipe_write_cmd(CMD_MASTER_SEC, 1, "m");
-    read(fd_sgx_ssl, buf, SSL3_MASTER_SECRET_SIZE);
-
-    return SSL3_MASTER_SECRET_SIZE;
-}
-
-
-void
-sgxbridge_rsa_sign_md(unsigned char* ip_md, int md_size, unsigned char* op_sig, int *sig_size)
+sgxbridge_get_master_secret(unsigned char* buf)
 {
-    sgxbridge_pipe_write_cmd(CMD_RSA_SIGN, md_size, ip_md);
+  sgxbridge_pipe_write_cmd(CMD_MASTER_SEC, 1, "m");
+  read(fd_sgx_ssl, buf, SSL3_MASTER_SECRET_SIZE);
 
-    read(fd_sgx_ssl, sig_size, sizeof(int));
-    read(fd_sgx_ssl, op_sig, *sig_size);
+  return SSL3_MASTER_SECRET_SIZE;
 }
 
 void
-sgxbridge_rsa_sign_sig_algo_ex(unsigned char* ip_md, int md_size, unsigned char* op_sig, int *sig_size)
+sgxbridge_rsa_sign_md(unsigned char* ip_md, int md_size, unsigned char* op_sig,
+                      int* sig_size)
 {
-	    sgxbridge_pipe_write_cmd(CMD_RSA_SIGN_SIG_ALG, md_size, ip_md);
+  sgxbridge_pipe_write_cmd(CMD_RSA_SIGN, md_size, ip_md);
 
-	    read(fd_sgx_ssl, sig_size, sizeof(int));
-	    read(fd_sgx_ssl, op_sig, *sig_size);
+  read(fd_sgx_ssl, sig_size, sizeof(int));
+  read(fd_sgx_ssl, op_sig, *sig_size);
+}
+
+void
+sgxbridge_rsa_sign_sig_algo_ex(unsigned char* ip_md, int md_size,
+                               unsigned char* op_sig, int* sig_size)
+{
+  sgxbridge_pipe_write_cmd(CMD_RSA_SIGN_SIG_ALG, md_size, ip_md);
+
+  read(fd_sgx_ssl, sig_size, sizeof(int));
+  read(fd_sgx_ssl, op_sig, *sig_size);
 }
