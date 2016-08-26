7797e9ed201405502c4b5479ef177013cf5c94e9
noname77 <wiktor.grajkowski@gmail.com>
2016-08-26 06:22:02 +0100

    added script to generate sloc differences

---
+++ build-sloc-diff.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# install cloc
+sudo apt-get -y install cloc 
+
+echo "Diffrence in source code lines for libressl:"
+
+cloc --quiet \
+     --exclude-lang=make,CMake,Assembly,'Bourne Again Shell','Bourne Shell',m4 \
+     --diff build/libressl-2.4.1.tar.gz build/libressl-2.4.1/
+
+echo "Diffrence in source code lines for nginx:"
+
+cloc --quiet \
+     --exclude-lang=make,CMake,Assembly,'Bourne Again Shell','Bourne Shell',m4,HTML,Perl,'vim script',SKILL,C++ \
+     --diff build/nginx-1.11.1.tar.gz build/nginx-1.11.1/
+
+#  save those (all languages) into a csv file
+cloc --quiet \
+     --exclude-lang=make,CMake,Assembly,'Bourne Again Shell','Bourne Shell',m4 \
+     --diff build/libressl-2.4.1.tar.gz build/libressl-2.4.1/ \
+     --report-file=./results/libressl_sloc_diff.csv \
+     --csv
+
+cloc --quiet \
+     --exclude-lang=make,CMake,Assembly,'Bourne Again Shell','Bourne Shell',m4,HTML,Perl,'vim script',SKILL,C++ \
+     --diff build/nginx-1.11.1.tar.gz build/nginx-1.11.1/ \
+     --report-file=./results/nginx_sloc_diff.csv \
+     --csv
