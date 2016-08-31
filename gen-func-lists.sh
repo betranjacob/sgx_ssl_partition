fcd814a65724830e38f18eaa270f8d16b4a03aa7
noname77 <wiktor.grajkowski@gmail.com>
2016-08-31 20:10:19 +0100

    comments

--- gen-func-lists.sh
+++ gen-func-lists.sh
@@ -2,6 +2,7 @@
 
 # process callgrind file to produce function list
 # ignores all functions starting with _, 0 or /
+# ifnores functions containing ., ?, ' and )
 
 CALLGRIND_PREFIX=callgrind.out
 declare -a arr=("stock" "outside" "inside")
@@ -15,13 +16,15 @@ for TEST_SUFFIX in "${arr[@]}"
 do
   CALLGRIND_FILE=$CALLGRIND_PREFIX.$TEST_SUFFIX
 
+  # make sure callgrind file exists
   if [ ! -f $CALLGRIND_FILE ]; then
   	echo "${CALLGRIND_FILE} does not exist."
     continue
   fi
 
-# cat $CALLGRIND_FILE | grep "cob\|cfl\|cfn\|fn" | grep " " | grep -o "[^ ]*$" | grep "^[^_0/]" | grep -v "[.?\']" | sort > function_list_${TEST_SUFFIX}_${now}.txt
-cat $CALLGRIND_FILE | grep "cob\|cfl\|cfn\|fn" | grep " " | grep -o "[^ ]*$" | grep "^[^_0/]" | grep -v "[.?\']" | sort > function_list_${TEST_SUFFIX}.txt
+# uncomment to add time of geration, useful if you dont want to overide results
+# cat $CALLGRIND_FILE | grep "cob\|cfl\|cfn\|fn" | grep " " | grep -o "[^ ]*$" | grep "^[^_0/]" | grep -v "[.?\']" | sort -u > function_list_${TEST_SUFFIX}_${now}.txt
+cat $CALLGRIND_FILE | grep "cob\|cfl\|cfn\|fn" | grep " " | grep -o "[^ ]*$" | grep "^[^_0/]" | grep -v "[.?\')]" | sort -u > function_list_${TEST_SUFFIX}.txt
 done
 
 cd ..
